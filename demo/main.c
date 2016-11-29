df930605f8e6dea2640c6d91fd7de0d2a17b7f05
Tank <Tank@tankdemacbook-pro.local>
2016-11-29 14:49:40 +0800

    Initial Commit

---
+++ demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  demo
+//
+//  Created by Tank on 16/11/29.
+//  Copyright © 2016年 Tank. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
